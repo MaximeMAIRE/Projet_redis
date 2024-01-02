f3dcb14d7edb0d0e2c51c20d0c09b299dd91a315
Mathéo <matomic@netcourrier.com>
2024-01-02 17:59:29 +0100

    updated some var and func names

--- sqlite_redis.py
+++ sqlite_redis.py
@@ -8,10 +8,10 @@ import plotly.io as pio
 import plotly.graph_objects as go
 
 
-def calc(tab):
+def compute_mean_and_std(tab):
     mean = np.average(tab)
     std = np.std(tab)
-    return mean,std
+    return mean, std
 
 def launch_all(nb_data):
     print("##################################################")
@@ -206,10 +206,10 @@ def launch_all(nb_data):
 
     r.flushall()
 
-    calc(tab_total_insert_sqlite)
-    calc(tab_retrieval_sqlite)
-    calc(tab_total_update_sqlite)
-    calc(tab_total_delete_sqlite)
+    compute_mean_and_std(tab_total_insert_sqlite)
+    compute_mean_and_std(tab_retrieval_sqlite)
+    compute_mean_and_std(tab_total_update_sqlite)
+    compute_mean_and_std(tab_total_delete_sqlite)
     return [[tab_insert_redis, tab_retrieval_redis, tab_update_redis, tab_delete_redis], 
             [tab_total_insert_sqlite, tab_first_insert_sqlite, tab_second_insert_sqlite, tab_retrieval_sqlite,
             tab_total_update_sqlite, tab_first_update_sqlite, tab_second_insert_sqlite, tab_total_delete_sqlite,
@@ -220,37 +220,37 @@ def launch_all(nb_data):
 tab un tableau
 int un entier compris dans [0;3]: 0 = insert; 1 = retrieval; 2 = update; 3 = delete
 '''
-def print_and_return_redis_perfs(tab,i):
-    mean, sd = calc(tab)
+def print_and_return_redis_perfs(tab, i):
+    mean, std = compute_mean_and_std(tab)
     if i == 0:
-        print(f"Temps d'insert en moyenne sur 1000 données sur Redis:             {mean:.8f} s. Écart-type : {sd:.8f}")
+        print(f"Temps d'insertion en moyenne sur 1000 données sur Redis:             {mean:.8f} s. Écart-type : {std:.8f}")
     elif i==1:
-        print(f"Temps de récup en moyenne sur 1000 données sur Redis:             {mean:.8f} s. Écart-type : {sd:.8f}")
+        print(f"Temps de récupération en moyenne sur 1000 données sur Redis:             {mean:.8f} s. Écart-type : {std:.8f}")
     elif i==2:
-        print(f"Temps de modif en moyenne sur 1000 données sur Redis:             {mean:.8f} s. Écart-type : {sd:.8f}")
+        print(f"Temps de modification en moyenne sur 1000 données sur Redis:             {mean:.8f} s. Écart-type : {std:.8f}")
     elif i==3:
-        print(f"Temps de suppr en moyenne sur 1000 données sur Redis:             {mean:.8f} s. Écart-type : {sd:.8f}")
-    return [mean, sd]
+        print(f"Temps de suppression en moyenne sur 1000 données sur Redis:             {mean:.8f} s. Écart-type : {std:.8f}")
+    return [mean, std]
 
-def print_and_return_sqlite_perfs(tabt,tabf,tabs,i):
-    meant, sdt = calc(tabt)
-    meanf, sdf = calc(tabf)
-    means, sds = calc(tabs)
+def print_and_return_sqlite_perfs(tab_total, tab_first, tab_second, i):
+    mean_total, std_total = compute_mean_and_std(tab_total)
+    mean_first, std_first = compute_mean_and_std(tab_first)
+    mean_second, std_second = compute_mean_and_std(tab_second)
     if i == 0:
-        print(f"Temps d'insert en moyenne sur 1000 données sur SQLite:             {meant:.8f} s. Écart-type : {sdt:.8f}")
-        print(f"Temps d'insert de la 1ere table en moyenne sur 1000 données sur SQLite: {meanf:.8f} s. Écart-type : {sdf:.8f}")
-        print(f"Temps d'insert de la 2eme table en moyenne sur 1000 données sur SQLite:  {means:.8f} s. Écart-type : {sds:.8f}")
+        print(f"Temps d'insertion en moyenne sur 1000 données sur SQLite:             {mean_total:.8f} s. Écart-type : {std_total:.8f}")
+        print(f"Temps d'insertion de la 1ère table en moyenne sur 1000 données sur SQLite: {mean_first:.8f} s. Écart-type : {std_first:.8f}")
+        print(f"Temps d'insertion de la 2ème table en moyenne sur 1000 données sur SQLite:  {mean_second:.8f} s. Écart-type : {std_second:.8f}")
     elif i == 1:
-        print(f"Temps de modif en moyenne sur 1000 données sur SQLite:             {meant:.8f} s. Écart-type : {sdt:.8f}")
-        print(f" Temps de modif de la 1ere table en moyenne sur 1000 données sur SQLite: {meanf:.8f} s. Écart-type : {sdf:.8f}")
-        print(f" Temps de modif de la 2eme table en moyenne sur 1000 données sur SQLite:  {means:.8f} s. Écart-type : {sds:.8f}")
+        print(f"Temps de modification en moyenne sur 1000 données sur SQLite:             {mean_total:.8f} s. Écart-type : {std_total:.8f}")
+        print(f" Temps de modification de la 1ere table en moyenne sur 1000 données sur SQLite: {mean_first:.8f} s. Écart-type : {std_first:.8f}")
+        print(f" Temps de modification de la 2eme table en moyenne sur 1000 données sur SQLite:  {mean_second:.8f} s. Écart-type : {std_second:.8f}")
     elif i == 2:
-        print(f"Temps de suppr en moyenne sur 1000 données sur SQLite:             {meant:.8f} s. Écart-type : {sdt:.8f}")
-        print(f" Temps de suppr de la 1ere table en moyenne sur 1000 données sur SQLite:  {meanf:.8f} s. Écart-type : {sdf:.8f}")
-        print(f" Temps de suppr de la 2eme table en moyenne sur 1000 données sur SQLite:   {means:.8f} s. Écart-type : {sds:.8f}\n\n")
-    return [meant, sdt]
+        print(f"Temps de suppression en moyenne sur 1000 données sur SQLite:             {mean_total:.8f} s. Écart-type : {std_total:.8f}")
+        print(f" Temps de suppression de la 1ere table en moyenne sur 1000 données sur SQLite:  {mean_first:.8f} s. Écart-type : {std_first:.8f}")
+        print(f" Temps de suppression de la 2eme table en moyenne sur 1000 données sur SQLite:   {mean_second:.8f} s. Écart-type : {std_second:.8f}\n\n")
+    return [mean_total, std_total]
 
-def mean_std(tab,tab2,x):
+def mean_std(tab, tab2, x):
     result = []
     if x == 0:
         for i in range(len(tab)):
@@ -282,33 +282,33 @@ sd_sqlite_delete = []
 
 for data in list_nb_data:
     perfs = launch_all(data)
-    x,y = print_and_return_redis_perfs(perfs[0][0],0)
-    mean_redis_insert.append(x)
-    sd_redis_insert.append(y)
-    x,y = print_and_return_redis_perfs(perfs[0][1],1)
-    mean_redis_retrieval.append(x)
-    sd_redis_retrieval.append(y)
-    x,y = print_and_return_redis_perfs(perfs[0][2],2)
-    mean_redis_update.append(x)
-    sd_redis_update.append(y)
-    x,y = print_and_return_redis_perfs(perfs[0][3],3)
-    mean_redis_delete.append(x)
-    sd_redis_delete.append(y)
+    mean, std = print_and_return_redis_perfs(perfs[0][0],0)
+    mean_redis_insert.append(mean)
+    sd_redis_insert.append(std)
+    mean, std = print_and_return_redis_perfs(perfs[0][1],1)
+    mean_redis_retrieval.append(mean)
+    sd_redis_retrieval.append(std)
+    mean, std = print_and_return_redis_perfs(perfs[0][2],2)
+    mean_redis_update.append(mean)
+    sd_redis_update.append(std)
+    mean, std = print_and_return_redis_perfs(perfs[0][3],3)
+    mean_redis_delete.append(mean)
+    sd_redis_delete.append(std)
 
     
-    x,y = calc(perfs[1][3])
-    print(f"Temps de récup en moyenne sur 1000 données sur SQLite:            {x:.8f} s. Écart-type : {y:.8f}")
-    mean_sqlite_retrieval.append(x)
-    sd_sqlite_retrieval.append(y)
-    x,y = print_and_return_sqlite_perfs(perfs[1][0],perfs[1][1],perfs[1][2],0)
-    mean_sqlite_insert.append(x)
-    sd_sqlite_insert.append(y)
-    x,y = print_and_return_sqlite_perfs(perfs[1][4],perfs[1][5],perfs[1][6],1)
-    mean_sqlite_update.append(x)
-    sd_sqlite_update.append(y)
-    x,y = print_and_return_sqlite_perfs(perfs[1][7],perfs[1][8],perfs[1][9],2)
-    mean_sqlite_delete.append(x)
-    sd_sqlite_delete.append(y)
+    mean, std = compute_mean_and_std(perfs[1][3])
+    print(f"Temps de récupération en moyenne sur 1000 données sur SQLite:            {mean:.8f} s. Écart-type : {std:.8f}")
+    mean_sqlite_retrieval.append(mean)
+    sd_sqlite_retrieval.append(std)
+    mean, std = print_and_return_sqlite_perfs(perfs[1][0],perfs[1][1],perfs[1][2],0)
+    mean_sqlite_insert.append(mean)
+    sd_sqlite_insert.append(std)
+    mean, std = print_and_return_sqlite_perfs(perfs[1][4],perfs[1][5],perfs[1][6],1)
+    mean_sqlite_update.append(mean)
+    sd_sqlite_update.append(std)
+    mean, std = print_and_return_sqlite_perfs(perfs[1][7],perfs[1][8],perfs[1][9],2)
+    mean_sqlite_delete.append(mean)
+    sd_sqlite_delete.append(std)
 
 sum_meanstd_redis_insert = mean_std(mean_redis_insert,sd_redis_insert,0)
 sum_meanstd_redis_retrieval = mean_std(mean_redis_retrieval,sd_redis_retrieval,0)
@@ -320,57 +320,57 @@ sum_meanstd_redis_update_neg = mean_std(mean_redis_update,sd_redis_update,1)
 sum_meanstd_redis_delete_neg = mean_std(mean_redis_delete,sd_redis_delete,1)
 
 liste_l = ['nb_data','Insertion',"Selection","Mise a jour","Suppression",
-           'Insertion_std_borne_supp',"Selection_std_borne_supp","Mise a jour_std_borne_supp","Suppression_std_borne_supp",
+           'Insertion_std_borne_sup',"Selection_std_borne_sup","Mise a jour_std_borne_sup","Suppression_std_borne_sup",
            'Insertion_std_borne_inf',"Selection_std_borne_inf","Mise a jour_std_borne_inf","Suppression_std_borne_inf"]
 
-d_redis_m = {
+data_redis = {
     'nb_data': list_nb_data,
     'Insertion': mean_redis_insert,
     'Selection': mean_redis_retrieval,
     'Mise a jour': mean_redis_update,
     'Suppression': mean_redis_delete,
-    'Insertion_std_borne_supp': sum_meanstd_redis_insert,
-    'Selection_std_borne_supp': sum_meanstd_redis_retrieval,
-    'Mise a jour_std_borne_supp': sum_meanstd_redis_update,
-    'Suppression_std_borne_supp': sum_meanstd_redis_delete,
+    'Insertion_std_borne_sup': sum_meanstd_redis_insert,
+    'Selection_std_borne_sup': sum_meanstd_redis_retrieval,
+    'Mise a jour_std_borne_sup': sum_meanstd_redis_update,
+    'Suppression_std_borne_sup': sum_meanstd_redis_delete,
     'Insertion_std_borne_inf': sum_meanstd_redis_insert_neg,
     'Selection_std_borne_inf': sum_meanstd_redis_retrieval_neg,
     'Mise a jour_std_borne_inf': sum_meanstd_redis_update_neg,
     'Suppression_std_borne_inf': sum_meanstd_redis_delete_neg
 }
 
-df_redis_m = pd.DataFrame(data=d_redis_m)
+df_redis = pd.DataFrame(data=data_redis)
 fig = go.Figure()
 
-fig.add_trace(go.Scatter(x=df_redis_m['nb_data'], y=df_redis_m['Insertion'],
+fig.add_trace(go.Scatter(x=df_redis['nb_data'], y=df_redis['Insertion'],
                          line=dict(color='blue'), mode='lines', name='Insertion'))
 
-fig.add_trace(go.Scatter(x=df_redis_m['nb_data'], y=df_redis_m['Suppression'],
+fig.add_trace(go.Scatter(x=df_redis['nb_data'], y=df_redis['Suppression'],
                          line=dict(color='red'), mode='lines', name='Suppression'))
 
-fig.add_trace(go.Scatter(x=df_redis_m['nb_data'], y=df_redis_m['Mise a jour'],
+fig.add_trace(go.Scatter(x=df_redis['nb_data'], y=df_redis['Mise a jour'],
                          line=dict(color='green'), mode='lines', name='Mise a jour'))
 
-fig.add_trace(go.Scatter(x=df_redis_m['nb_data'], y=df_redis_m['Selection'],
+fig.add_trace(go.Scatter(x=df_redis['nb_data'], y=df_redis['Selection'],
                          line=dict(color='purple'), mode='lines', name='Récupération'))
 
-fig.add_trace(go.Scatter(x=df_redis_m['nb_data'].tolist() + df_redis_m['nb_data'].tolist()[::-1],
-                         y=df_redis_m['Insertion_std_borne_supp'].tolist() + df_redis_m['Insertion_std_borne_inf'].tolist()[::-1],
+fig.add_trace(go.Scatter(x=df_redis['nb_data'].tolist() + df_redis['nb_data'].tolist()[::-1],
+                         y=df_redis['Insertion_std_borne_sup'].tolist() + df_redis['Insertion_std_borne_inf'].tolist()[::-1],
                          fill='toself', fillcolor='rgba(0,100,80,0.2)',
                          line=dict(color='rgba(255,255,255,0)'), name='Insertion Intervalle'))
 
-fig.add_trace(go.Scatter(x=df_redis_m['nb_data'].tolist() + df_redis_m['nb_data'].tolist()[::-1],
-                         y=df_redis_m['Suppression_std_borne_supp'].tolist() + df_redis_m['Suppression_std_borne_inf'].tolist()[::-1],
+fig.add_trace(go.Scatter(x=df_redis['nb_data'].tolist() + df_redis['nb_data'].tolist()[::-1],
+                         y=df_redis['Suppression_std_borne_sup'].tolist() + df_redis['Suppression_std_borne_inf'].tolist()[::-1],
                          fill='toself', fillcolor='rgba(255,0,0,0.2)',
                          line=dict(color='rgba(255,255,255,0)'), name='Suppression Intervalle'))
 
-fig.add_trace(go.Scatter(x=df_redis_m['nb_data'].tolist() + df_redis_m['nb_data'].tolist()[::-1],
-                         y=df_redis_m['Mise a jour_std_borne_supp'].tolist() + df_redis_m['Mise a jour_std_borne_inf'].tolist()[::-1],
+fig.add_trace(go.Scatter(x=df_redis['nb_data'].tolist() + df_redis['nb_data'].tolist()[::-1],
+                         y=df_redis['Mise a jour_std_borne_sup'].tolist() + df_redis['Mise a jour_std_borne_inf'].tolist()[::-1],
                          fill='toself', fillcolor='rgba(0,255,0,0.2)',
                          line=dict(color='rgba(255,255,255,0)'), name='Mise a jour Intervalle'))
 
-fig.add_trace(go.Scatter(x=df_redis_m['nb_data'].tolist() + df_redis_m['nb_data'].tolist()[::-1],
-                         y=df_redis_m['Selection_std_borne_supp'].tolist() + df_redis_m['Selection_std_borne_inf'].tolist()[::-1],
+fig.add_trace(go.Scatter(x=df_redis['nb_data'].tolist() + df_redis['nb_data'].tolist()[::-1],
+                         y=df_redis['Selection_std_borne_sup'].tolist() + df_redis['Selection_std_borne_inf'].tolist()[::-1],
                          fill='toself', fillcolor='rgba(128,0,128,0.2)',
                          line=dict(color='rgba(255,255,255,0)'), name='Récupération Intervalle'))
 
@@ -392,16 +392,16 @@ sum_meanstd_sqlite_retrieval_neg = mean_std(mean_sqlite_retrieval,sd_sqlite_retr
 sum_meanstd_sqlite_update_neg = mean_std(mean_sqlite_update,sd_sqlite_update,1)
 sum_meanstd_sqlite_delete_neg = mean_std(mean_sqlite_delete,sd_sqlite_delete,1)
 
-d_sqlite = {
+data_sqlite = {
     'nb_data': list_nb_data,
     'Insertion': mean_sqlite_insert, 
     'Selection': mean_sqlite_retrieval, 
     'Mise a jour': mean_sqlite_update, 
     'Suppression': mean_sqlite_delete,
-    'Insertion_std_borne_supp': sum_meanstd_sqlite_insert,
-    'Selection_std_borne_supp': sum_meanstd_sqlite_retrieval,
-    'Mise a jour_std_borne_supp': sum_meanstd_sqlite_update,
-    'Suppression_std_borne_supp': sum_meanstd_sqlite_delete,
+    'Insertion_std_borne_sup': sum_meanstd_sqlite_insert,
+    'Selection_std_borne_sup': sum_meanstd_sqlite_retrieval,
+    'Mise a jour_std_borne_sup': sum_meanstd_sqlite_update,
+    'Suppression_std_borne_sup': sum_meanstd_sqlite_delete,
     'Insertion_std_borne_inf': sum_meanstd_sqlite_insert_neg,
     'Selection_std_borne_inf': sum_meanstd_sqlite_retrieval_neg,
     'Mise a jour_std_borne_inf': sum_meanstd_sqlite_update_neg,
@@ -409,7 +409,7 @@ d_sqlite = {
 }
 
 
-df_sqlite = pd.DataFrame(data=d_sqlite)
+df_sqlite = pd.DataFrame(data=data_sqlite)
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(x=df_sqlite['nb_data'], y=df_sqlite['Insertion'],
@@ -425,27 +425,27 @@ fig.add_trace(go.Scatter(x=df_sqlite['nb_data'], y=df_sqlite['Selection'],
                          line=dict(color='purple'), mode='lines', name='Récupération'))
 
 fig.add_trace(go.Scatter(x=df_sqlite['nb_data'].tolist() + df_sqlite['nb_data'].tolist()[::-1],
-                         y=df_sqlite['Insertion_std_borne_supp'].tolist() + df_sqlite['Insertion_std_borne_inf'].tolist()[::-1],
+                         y=df_sqlite['Insertion_std_borne_sup'].tolist() + df_sqlite['Insertion_std_borne_inf'].tolist()[::-1],
                          fill='toself', fillcolor='rgba(0,100,80,0.2)',
                          line=dict(color='rgba(255,255,255,0)'), name='Insertion Intervalle'))
 
 fig.add_trace(go.Scatter(x=df_sqlite['nb_data'].tolist() + df_sqlite['nb_data'].tolist()[::-1],
-                         y=df_sqlite['Suppression_std_borne_supp'].tolist() + df_sqlite['Suppression_std_borne_inf'].tolist()[::-1],
+                         y=df_sqlite['Suppression_std_borne_sup'].tolist() + df_sqlite['Suppression_std_borne_inf'].tolist()[::-1],
                          fill='toself', fillcolor='rgba(255,0,0,0.2)',
                          line=dict(color='rgba(255,255,255,0)'), name='Suppression Intervalle'))
 
 fig.add_trace(go.Scatter(x=df_sqlite['nb_data'].tolist() + df_sqlite['nb_data'].tolist()[::-1],
-                         y=df_sqlite['Mise a jour_std_borne_supp'].tolist() + df_sqlite['Mise a jour_std_borne_inf'].tolist()[::-1],
+                         y=df_sqlite['Mise a jour_std_borne_sup'].tolist() + df_sqlite['Mise a jour_std_borne_inf'].tolist()[::-1],
                          fill='toself', fillcolor='rgba(0,255,0,0.2)',
                          line=dict(color='rgba(255,255,255,0)'), name='Mise a jour Intervalle'))
 
 fig.add_trace(go.Scatter(x=df_sqlite['nb_data'].tolist() + df_sqlite['nb_data'].tolist()[::-1],
-                         y=df_sqlite['Selection_std_borne_supp'].tolist() + df_sqlite['Selection_std_borne_inf'].tolist()[::-1],
+                         y=df_sqlite['Selection_std_borne_sup'].tolist() + df_sqlite['Selection_std_borne_inf'].tolist()[::-1],
                          fill='toself', fillcolor='rgba(128,0,128,0.2)',
                          line=dict(color='rgba(255,255,255,0)'), name='Récupération Intervalle'))
 
 fig.update_layout(
-    title='Performance evolution with redis',
+    title='Performance evolution with SQLite',
     xaxis_title='nb_data',
     yaxis_title='Values',
 )
